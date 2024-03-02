799155a5a1a75749035feb906d473a8b2b323711
lucas ludueña <lucas.ludu.mdp@hotmail.com>
2024-03-01 23:55:19 -0300

    Registro Login y Get de usuario

--- MiPresupuestoApp.Api/Controllers/UsuarioController.cs
+++ MiPresupuestoApp.Api/Controllers/UsuarioController.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MiPresupuestoApp.Api.Services.Interface;
 using MiPresupuestoApp.Models.DTO;
@@ -23,13 +22,48 @@ namespace MiPresupuestoApp.Api.Controllers
         }
 
 
-        [HttpPost]
+        [HttpPost("Register")]
         public ActionResult Register([FromForm]UserRegisterDto userRegisterDto)
         {
             try
             {
                 var usuario = _service.Register(userRegisterDto, userRegisterDto.Password);
-                return Ok(usuario);
+                return (usuario == null)
+                    ? StatusCode(StatusCodes.Status201Created, usuario)
+                    : StatusCode(StatusCodes.Status400BadRequest, "No se pudo registrar");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Login([FromForm]UserLoginDto userLoginDto)
+        {
+            try
+            {
+                var usuario = _service.Login(userLoginDto);
+                return (usuario != null)
+                    ? StatusCode(StatusCodes.Status202Accepted, _service.GetToken(usuario))
+                    : StatusCode(StatusCodes.Status203NonAuthoritative, "Usuario y/o contraseña Incorreta");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Get(string correo)
+        {
+            try
+            {
+                var usuario = _unitOfWork.Usuario.GetByCondition(a => a.Correo.Equals(correo));
+                return (usuario != null)
+                    ? StatusCode(StatusCodes.Status200OK, _mapper.Map<UserDto>(usuario))
+                    : StatusCode(StatusCodes.Status400BadRequest, "El usuario ingresado no se encuentra");
             }
             catch (Exception ex)
             {
